56b49708919a32dc52a3b3694636c305efdb084e
VFDGROUPLTD\Ayomide.Akinrotoye <ayomide.akinrotoye@vfdtech.ng>
2021-10-16 21:32:40 +0100

    removed unneccessary imports

--- src/main/java/com/pangaea/publisher/services/SubscriberImpl.java
+++ src/main/java/com/pangaea/publisher/services/SubscriberImpl.java
@@ -1,18 +1,9 @@
 package com.pangaea.publisher.services;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.pangaea.publisher.models.NotificationMessage;
 import com.pangaea.publisher.models.Subscriber;
 import com.pangaea.publisher.models.SubscriptionUrl;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
-@RequiredArgsConstructor
 public class SubscriberImpl extends Subscriber {
-	private ObjectMapper objectMapper;
 	
 	@Override
 	public void unSubscribe(String topic, PublisherSubscriberService publisherService) {
@@ -28,17 +19,6 @@ public class SubscriberImpl extends Subscriber {
 	@Override
 	public void addSubscriber(String topic, SubscriptionUrl subscriptionUrl, PublisherSubscriberService publisherService) {
 		publisherService.addSubscriber(topic, subscriptionUrl, this);
-		
 	}
-	
-	public void printMessages() throws JsonProcessingException {
-		for (NotificationMessage message : getSubscriberMsgs()) {
-			if(objectMapper == null) {
-				objectMapper = new ObjectMapper();
-			}
-			log.info(objectMapper.writeValueAsString(message));
-		}
-	}
-
 
 }
